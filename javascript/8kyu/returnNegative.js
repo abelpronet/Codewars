f96d611dfa5c010dcece683dd74d5e75c9fa2750
Abel Colmenares Napolitano <abelpronet@gmail.com>
2019-04-30 01:59:18 +0200

    update

---
+++ javascript/8kyu/returnNegative.js
@@ -0,0 +1,31 @@
+/*
+Codewars Kata Details:
+
+    - Title: Return Negative
+    - Difficulty: 8 kyu
+    - Link: https://www.codewars.com/kata/return-negative
+    - Language: Javascript
+
+Description: 
+
+    In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Example:
+
+    makeNegative(1); // return -1
+    makeNegative(-5); // return -5
+    makeNegative(0); // return 0
+    makeNegative(0.12); // return -0.12
+
+Notes:
+
+  The number can be negative already, in which case no change is required.
+  Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+*/
+
+// Solution
+
+const makeNegative = num => num < 0 ? num : - num
+
+makeNegative(-1) // -1
+// makeNegative(1) // -1
